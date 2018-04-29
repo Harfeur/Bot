fd64cd014ffaac189a5a0e7378c13f7b2e4b18ca
Maxime Chourré <maxou3153@gmail.com>
2018-04-29 21:20:40 +0200

    ajout tuto tb

--- index.js
+++ index.js
@@ -107,13 +107,13 @@ const commands = {
 		msg.channel.send('Une fois terminée, vous devez poster la feuille de route dans le canal <#424673120532561922> et les comptables s\'en occuperont !', {
 			files: [{
 				attachment: 'assets/Feuille_de_route.xlsx',
-				name: 'Feuille_de_route.xlsx'
+				name: 'Feuille de route.xlsx'
 			}]
 		});
 		msg.channel.send({
 			files: [{
 				attachment: 'assets/Comment_faire_une_feuille_de_route.pdf',
-				name: 'Comment_faire_une_feuille_de_route.pdf'
+				name: 'Comment faire une feuille de route.pdf'
 			}]
 		}, )
 	},
@@ -121,11 +121,12 @@ const commands = {
 		msg.channel.send('https://drive.google.com/open?id=17iusTta_JgnQXh35GMUxoYXOMRqXa7xuD_QoUjJRkIk');
 	},
 	'tb': (msg) => {
-		msg.channel.send({
-				files: ['https://cdn.discordapp.com/attachments/399979689558409237/432236647959101441/Tutoriel_TruckBook.pdf']
-			})
-			.catch(console.error);
-		msg.channel.send('http://trucksbook.eu/');
+		msg.channel.send('http://trucksbook.eu/',{
+			files: [{
+				attachment: 'assets/Tutoriel_TruckBook.pdf',
+				name: 'Tutoriel TruckBook.pdf'
+			}]
+		});
 	},
 	'accident': (msg) => {
 		msg.channel.send('```Vous avez eu un accident ?! Vous allez bien ?!\n\nPour les constats, rendez-vous dans le canal #feuilles-de-route. Ensuite, vous devez mettre la date, l\'heure à laquelle c\'est arrivée et une description détaillée de ce qui est arrivé (photos conseillées).\n\n/!\\ MAIS ATTENTION /!\\\nPour repartir il faut respecter l\'article du règlement qui dit : \" En cas de dégâts importants, c’est à dire plus de 15 % vous devrez rejoindre le garage le plus proche et faire une demande d’assistance. Vous devrez également annuler votre mission actuelle. Après réception de cette demande, une démarche sera engagé pour faire revenir le camion à l’entreprise. Vous devrez alors rejoindre un des garages de l’entreprise, le plus proche. Vous ne devez en aucun cas continuer de rouler avec le camion abîmé et cela pour se rapprocher le plus à la réalité. \"```');
