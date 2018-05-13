fd2d2a0b57c6efee27448bc9e06cd4e45f30a151
Maxime Chourré <maxou3153@gmail.com>
2018-05-13 14:41:33 +0200

    update radios

--- index.js
+++ index.js
@@ -128,9 +128,7 @@ const commands = {
 				break;
 			case 'tfm':
 				msg.member.voiceChannel.join().then(connection => {
-					require('http').get("https://radio.truckers.fm", (res) => {
-						connection.playStream(res, { volume: 0.1 });
-					})
+						connection.playArbitraryInput('https://radio.truckers.fm', { volume: 0.1 });
 				})
 					.catch(console.error);
 				break;
