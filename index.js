e22ffbc818443e00170ea1e09bf1721ff9890c09
Maxime Chourré <maxou3153@gmail.com>
2018-06-20 10:08:41 +0200

    tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const yt = require('ytdl-core');
+const fs = require("fs");
 new Discord.RichEmbed();
 
 client.commands = new Discord.Collection();
