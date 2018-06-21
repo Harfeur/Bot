4ca5ce8cfc95c021a29be7cb59649438430050cd
Maxime Chourré <maxou3153@gmail.com>
2018-06-21 18:33:42 +0200

    spam off

--- index.js
+++ index.js
@@ -299,8 +299,6 @@ client.on('message', async msg => {
 			.catch(console.error);
 	}
 
-		msg.channel.send('SPAM');
-
 	if (msg.channel.name === "feuilles-de-route") {
 		fichiers = msg.attachments.array();
 		console.log(fichiers);
