1df33338bc0bc215d44939ff330bd1d8c6197fdd
Maxime Chourré <maxou3153@gmail.com>
2018-06-23 17:27:58 +0200

    2.0a

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.cmds = new Discord.Collection();
-
-var prefix = process.env.prefix;
 
 const fs = require('fs');
 
@@ -12,31 +9,30 @@ client.on('ready', async () => {
     client.user.setActivity('Maintenance', {type: 'PLAYING'});
     console.log("Bot prêt !");
 });
-
-fs.readdir("./commandes/", (err, files) => {
-    if (err) throw (err);
-
-    let jsf = files.filter(f => f.split(".").pop() === "js");
-
-    if (jsf.lenght == 0) return;
-
-    jsf.forEach((f, i) => {
-        let props = require("./commandes/${f}");
-        client.cmds.set(props.help.name, props);
+/*
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        let commandFunction = require('./commands/${file}');
+        let commandName = file.split(".")[0];
+        client.on(commandName, (...args) => commandFunction.run(client, ...args));
     });
 });
-
+*/
 client.on('message', message => {
 
 
 
-    if (message.content.indexOf(prefix) !== 0) return;
-    let msg = message.content.split(" ");
-    let cmd = msg[0].toLowerCase();
-    let args = msg.slice(1);
+    if (message.content.indexOf(process.env.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    cmdf = client.cmds.get(cmd.slice(prefix.lenght));
-    if (cmdf) cmdf.run(client, message, args)
+    try {
+        let commandFile = require(`./commands/${command}.js`);
+        commandFile.run(client, message, args);
+      } catch (err) {
+        console.error(err);
+      }
 });
 
 client.login(process.env.token);
