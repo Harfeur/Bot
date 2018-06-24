48a72c0116338c9a0edf67c8aa4ad803e6e3f3d8
Maxime Chourré <maxou3153@gmail.com>
2018-06-24 15:18:16 +0200

    2.1

--- guilds/374826248259764237/commands/purge.js
+++ guilds/374826248259764237/commands/purge.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    if (message.author.hasPermission('MANAGE_MESSAGES')) {
+    if (message.member.hasPermission('MANAGE_MESSAGES')) {
         if (args[0] === undefined || args[0] < 2 || args[0] > 100)
             return message.reply("Merci de donner un nombre de 2 à 100 messages à supprimer.");
         const fetched = await message.channel.fetchMessages({
