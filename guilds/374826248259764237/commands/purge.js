76c6bda1bdfd6b251312c33de2b784284d9d634d
Maxime Chourré <maxou3153@gmail.com>
2018-06-24 15:16:41 +0200

    2.1

--- guilds/374826248259764237/commands/purge.js
+++ guilds/374826248259764237/commands/purge.js
@@ -1,10 +1,9 @@
-exports.run = async (client, message) => {
-    if (message.author.id == process.env.MaxouCraft || message.author.id == process.env.Teddy) {
-        const deleteCount = parseInt(args[0], 10);
-        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+exports.run = async (client, message, args) => {
+    if (message.author.hasPermission('MANAGE_MESSAGES')) {
+        if (args[0] === undefined || args[0] < 2 || args[0] > 100)
             return message.reply("Merci de donner un nombre de 2 à 100 messages à supprimer.");
         const fetched = await message.channel.fetchMessages({
-            limit: deleteCount
+            limit: args[0]
         });
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Erreur: ${error}`));
